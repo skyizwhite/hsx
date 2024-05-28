6daffb4ddfe7e3716fd7a3c9e83b2aadaf7858cc
paku <paku@skyizwhite.dev>
2024-05-28 16:13:41 +0900

    Improve hsx-test/hsx

--- tests/hsx.lisp
+++ tests/hsx.lisp
@@ -2,11 +2,6 @@
   (:use #:cl
         #:fiveam
         #:hsx/hsx)
-  (:import-from #:hsx/element
-                #:element-type
-                #:element-children)
-  (:import-from #:hsx/defhsx
-                #:defcomp)
   (:import-from #:hsx/builtin))
 (in-package #:hsx-test/hsx)
 
@@ -14,20 +9,17 @@
 (def-suite hsx-test)
 (in-suite hsx-test)
 
-(defcomp div (&rest props)
-  (declare (ignore props))
-  "This is fake!")
-
-(defcomp p (&rest props)
-  (declare (ignore props))
-  "This is fake!")
-
 (test find-symbols
-  (let ((fake-elm (div :prop "value"
-                    (p "brah"))))
-    (is (eql (element-type fake-elm) #'%div)
-        (eql (element-type (first (element-children fake-elm))) #'%p)))
-  (let ((true-elm (hsx (div :prop "value"
-                         (p "brah")))))
-    (is (equal (element-type true-elm) "div")
-        (equal (element-type (first (element-children true-elm))) "p"))))
+  (is (equal (macroexpand-1
+              '(hsx (div
+                      div
+                      (div
+                        div
+                        (div))
+                      div)))
+             '(hsx/builtin:div
+               div
+               (hsx/builtin:div
+                div
+                (hsx/builtin:div))
+               div))))
